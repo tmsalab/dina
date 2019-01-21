6389e3cf410542cda525688f6c6d9a2560b60ca4
James Balamuta <balamut2@illinois.edu>
2019-01-21 11:09:09 -0600

    Move re-exports to separate file

--- R/dina-package.R
+++ R/dina-package.R
@@ -1,13 +1,4 @@
 #' @useDynLib "dina", .registration=TRUE
 #' @importFrom Rcpp evalCpp
+#' @aliases dina-package
 "_PACKAGE"
-
-# -------- Model Simulation
-
-#' @importFrom simcdm sim_dina_items
-#' @export
-simcdm::sim_dina_items
-
-#' @importFrom simcdm sim_alpha_matrix
-#' @export
-simcdm::sim_alpha_matrix#' @aliases dina-package
