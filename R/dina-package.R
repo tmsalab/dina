45257c9d05eddf974fd9992b48328f358901190d
James Balamuta <balamut2@illinois.edu>
2019-01-21 11:55:22 -0600

    Switch to a dependency on simcdm to automatically get the simulation methods.

--- R/dina-package.R
+++ R/dina-package.R
@@ -1,4 +1,5 @@
 #' @useDynLib "dina", .registration=TRUE
 #' @importFrom Rcpp evalCpp
+#' @importFrom simcdm sim_dina_items sim_attribute_classes sim_q_matrix sim_subject_attributes
 #' @aliases dina-package
 "_PACKAGE"
--- R/re-exports.R
+++
@@ -1,9 +0,0 @@
-# -------- Model Simulation
-
-#' @importFrom simcdm sim_dina_items
-#' @export
-simcdm::sim_dina_items
-
-#' @importFrom simcdm sim_attribute_classes
-#' @export
-simcdm::sim_attribute_classes
